5cb212287d29fe650caafa5e8fedbefc92714d60
Robert Stiff <uatecuk@gmail.com>
2016-05-05 23:48:37 +0100

    Pull mongo collection name from env vars.

--- server/mongo-productsApi.js
+++ server/mongo-productsApi.js
@@ -3,11 +3,12 @@ var router = new express.Router();
 
 var MongoClient = require('mongodb').MongoClient;
 var url = GLOBAL.env.productsMongoUrl;
+var collectionName = GLOBAL.env.products_mongo_collection;
 
 router
     .get('/', function (req, res, next) {
         MongoClient.connect(url, function(err, db) {
-            var products = db.collection('products');
+            var products = db.collection(collectionName);
             products.find({}).toArray(function(err, docs) {
                 if ( err ) {
                     throw new Error(err);
