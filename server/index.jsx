be08048be7d8fbad6c6b85b4307811f780af70f3
Robert Stiff <uatecuk@gmail.com>
2016-05-05 23:27:00 +0100

    env file is only really for dev, so make it optional. other environments will use environment variables some other way

--- server/index.jsx
+++ server/index.jsx
@@ -7,7 +7,7 @@ var path = require('path');
 var _ = require('lodash');
 
 var env = require('node-env-file');
-env('./.env');
+env('./.env', {raise: false});
 
 var envVars = '<script>GLOBAL = {}; GLOBAL.env=' + JSON.stringify(_.pick(process.env, [
   'enable_menus', 'stub_menus', 'contentful_space_id', 'contentful_access_token'
