470cf2b5a45a30554b03027b4bc22245df084dca
Robert Stiff <uatecuk@gmail.com>
2016-05-12 23:32:03 +0100

    Query for tags based on the easier mechanism to merge existing tag documents.

--- server/mongo-productsApi.js
+++ server/mongo-productsApi.js
@@ -8,9 +8,15 @@ var collectionName = GLOBAL.env.products_mongo_collection;
 router
     .get('/', function (req, res, next) {
         MongoClient.connect(url, function(err, db) {
-            var query = {tags: {
-                $in: JSON.parse(req.query.q) || []
-            }};
+            
+            var query = {};
+            
+            var tags = JSON.parse(req.query.q) || [];
+            for ( var i in tags ) {
+                var tag = tags[i];
+                query['tags.' + tag] = true;
+            }
+            
             console.log('QUERY - ' + JSON.stringify(req.params.tags) + ' -> ' + JSON.stringify(query));
             var products = db.collection(collectionName);
             products.find(query).toArray(function(err, docs) {
