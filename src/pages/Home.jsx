c8968dc4e5cfb8b7f71fd8071fd4dd1afd6d1a63
Robert Stiff <uatecuk@gmail.com>
2016-05-13 22:31:33 +0100

    Show menus on the home page for an introduction to navigation.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,50 @@ var connect = require('react-redux').connect;
 
 var Link = require('react-router').Link;
 
-var NoMatch = React.createClass({
+var mapStateToProps = function (state) {
+    return {
+        categories: state.reducers.categories,
+        themeGroups: state.reducers.themeGroups
+    };
+};
+
+var Home = React.createClass({
 
     render: function () {
 
+        var categories = this.props.categories ? this.props.categories.map(function (c) {
+            return <div key={c.id}>
+                <Link to={'/category/' + c.id}>
+                    {c.name}
+                </Link>
+            </div>;
+        }) : [];
+
+        var themeGroups = this.props.themeGroups ? this.props.themeGroups.map(function (tg) {
+            return <div key={tg.id}>
+                <Link to={'/theme/' + tg.id}>
+                    {tg.name}
+                </Link>
+            </div>;
+        }) : [];
+
+
         return <div>
+            <div>
+                Shop by Category
+                {categories}
+            </div>
+            <div>
+                Shop by Room
+                {themeGroups}
+            </div>
+
             <h2>Welcome</h2>
             <h3>Try using the menus</h3>
         </div>;
     }
 });
 
-module.exports = NoMatch;
+module.exports = connect(
+    mapStateToProps
+)(Home);
